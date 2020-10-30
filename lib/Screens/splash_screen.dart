68da98b6d7c29f505b49c5f1619ea29606548ec0
kiranraj <62588175+kiranraj62@users.noreply.github.com>
2020-10-30 13:49:11 +0530

    Splash Sceen

--- lib/Screens/splash_screen.dart
+++ lib/Screens/splash_screen.dart
@@ -4,15 +4,12 @@ class SplashScreen extends StatelessWidget {
   static String id = 'SplashScreen Id';
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: SafeArea(
-          child: Center(
-            child: Column(children: [
-              Image.asset('assets/splash.png'),
-            ]),
-          ),
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(children: [
+            Image.asset('assets/splash.png'),
+          ]),
         ),
       ),
     );
